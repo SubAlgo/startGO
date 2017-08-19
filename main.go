de46e13f4c70b3504b14487e9366a30ebf6ab4e6
kornchaval <kornchaval@gmail.com>
2017-08-20 02:03:33 +0700

    CheckTypeInterface

--- main.go
+++ main.go
@@ -2,50 +2,33 @@ package main
 
 import "fmt"
 
-//-----------PERSON-----------------
-type person struct {
-	Name     string
-	NickName string
-}
-
-func (p person) Talk() {
-	fmt.Println("Hello, I'm ", p.Name)
-	fmt.Println("My nickname is ", p.NickName)
-}
-
-//-----------PERSON-----------------
-
-//-----------CAT------------------
-type cat struct{}
-
-func (cat) Talk() { //เหมือนเป็นการระบุว่า นี้เป็น Method ของ cat
-	fmt.Println("Nyaa nyaa")
-}
-
-//-----------CAT------------------
-
-//-----------DOG------------------
-type dog struct{}
-
-func (*dog) Talk() {
-	fmt.Println("Wan Wan")
-}
-
-//-----------DOG------------------
-
-type talkable interface {
-	Talk()
-}
-
-func talkWith(t talkable) { //talkWith implement talkable
-	t.Talk()
-}
-
 func main() {
-	p := person{"SubAlgo", "Sual"} //Create p
-	talkWith(p)                    //call func(method) talkWith and send paran (p)
-	talkWith(cat{})                //call func(method) talkWith and send paran (cat{})
-	talkWith(&dog{})               //call func(method) talkWith and send paran (&dog{})
-	d := dog{}
-	talkWith(&d)
+	checkType("SubAlgo")
+	checkType(10)
+	checkType(true)
+
+	checkType2("SubAlgo")
+	checkType2(10)
+	checkType2(true)
+}
+
+func checkType(v interface{}) {
+	switch p := v.(type) {
+	case string:
+		fmt.Println("String: ", p)
+	case int:
+		fmt.Println("Int: ", p+10)
+	case bool:
+		fmt.Println("Bool: ", p)
+	}
+}
+
+func checkType2(v interface{}) {
+	param, ok := v.(string)
+
+	if ok {
+		fmt.Println("Param is string: ", param)
+	} else {
+		fmt.Println("Param is not string!!")
+	}
 }
