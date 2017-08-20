1162f98ea33ecba0ac6a335d21612fce0246a825
kornchaval <kornchaval@gmail.com>
2017-08-20 19:45:44 +0700

    Basic GoRutines and Chanel

--- main.go
+++ main.go
@@ -1,24 +1,63 @@
 package main
 
+//Basic GoRutines and Chanel
 import (
 	"fmt"
 	"time"
 )
 
-// GOROUTINES != THREADS
-func main() {
-	fmt.Println("Start...")
-	go talk("Hello")
-	go talk("hi")
-	fmt.Println("Waiting")
-	time.Sleep(5 * time.Second)
-	fmt.Println("***END***")
+var (
+	arr1 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	arr2 = []int{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+)
 
+func main() {
+	fmt.Println("WithGo")
+	withGo()
+	fmt.Println("WithOutGo")
+	withOutGo()
 }
 
-func talk(prefix string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(prefix, i)
-		time.Sleep(time.Second)
+//function withGo
+func withGo() {
+	defer timer()()
+	chRes1 := make(chan int) //Create Chanel
+	chRes2 := make(chan int) //Create Chanel
+
+	go func() {
+		chRes1 <- sum(arr1)
+	}()
+
+	go func() {
+		chRes2 <- sum(arr2)
+	}()
+
+	fmt.Println("Sum arr1: ", <-chRes1)
+	fmt.Println("Sum arr2: ", <-chRes2)
+}
+
+//Function withOutGo
+func withOutGo() {
+	defer timer()()
+	fmt.Println("Sum arr1: ", sum(arr1))
+	fmt.Println("Sum arr2: ", sum(arr2))
+}
+
+//Function timmer
+func timer() func() {
+	t := time.Now()
+	return func() {
+		diff := time.Now().Sub(t)
+		fmt.Println(diff)
 	}
+}
+
+//Function sum
+func sum(a []int) int {
+	s := 0
+	for _, x := range a {
+		s += x
+		time.Sleep(time.Millisecond * 200)
+	}
+	return s
 }
