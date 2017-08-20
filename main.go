29c1229f32916cafb437c490dc503b7e277d753b
kornchaval <kornchaval@gmail.com>
2017-08-21 00:34:11 +0700

    Basic WebServer1

--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
-// GoRutine and Chanel CheckWorkWithTimeLimit
 import (
-	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
 func main() {
-	//res := doVerryLongWork()
-	res, err := doWorkWithLimitTime(2 * time.Second)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(res)
+	err := http.ListenAndServe(":8080", &indexHandler{})
+	log.Println(err)
 }
 
-func doVerryLongWork() int {
-	time.Sleep(4 * time.Second)
-	return 1
+type indexHandler struct{}
+
+func (*indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, SubAlgo"))
 }
 
-func doWorkWithLimitTime(d time.Duration) (int, error) {
-	ch := make(chan int)
-	go func() {
-		ch <- doVerryLongWork()
-	}()
-
-	select { //ถ้า chanel ไหนเสร็จก่อนก็ให้ทำ chanel นั้น
-	case r := <-ch: //ch ทำงานเสร็จก็ให้เอามาใส่ในตัวแปร r
-		return r, nil
-	case <-time.After(d): //หลังเวลาที่กำหนด (d) ให้ทำ statement นี้ เช่นค่าที่ส่งมา 5 * time.Second คือ หลังจาก 5วิ
-		return 0, fmt.Errorf("timeout")
-	}
-}
+//28
