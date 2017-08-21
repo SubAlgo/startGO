a69b2bfa7abb1370858e29d8213c4d3d6d802077
kornchaval <kornchaval@gmail.com>
2017-08-22 00:51:21 +0700

    Mutiplexer style2

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "net/http"
 import "log"
 
-//Mutiplexer (MUX)
+//Mutiplexer style2 (MUX)
+//MUX วิธีนี้โดยค่าเริ่มต้นถ้าไม่มีurl จะไปที่ Index
+//Code ที่ commend เป็นแบบวิธีที่1
+
+/*
 func mux(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
@@ -17,10 +21,22 @@ func mux(w http.ResponseWriter, r *http.Request) {
 		//notFoudHandler(w, r)
 	}
 }
+*/
 
+/*
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Index Page"))
 }
+*/
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Index Page"))
+}
+
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("About Page"))
 }
@@ -32,7 +48,13 @@ func notFoudHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	h := http.HandlerFunc(mux)
-	err := http.ListenAndServe(":8080", h)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/login", loginHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+	//h := http.HandlerFunc(mux)
+	//err := http.ListenAndServe(":8080", h)
 	log.Println(err)
 }
