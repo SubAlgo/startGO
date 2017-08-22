cc20628997933dfaafe3ba7a9706b4dedea185f7
kornchaval <kornchaval@gmail.com>
2017-08-22 23:14:28 +0700

    Basic Middleware

--- main.go
+++ main.go
@@ -1,60 +1,29 @@
 package main
 
-import "net/http"
-import "log"
-
-//Mutiplexer style2 (MUX)
-//MUX วิธีนี้โดยค่าเริ่มต้นถ้าไม่มีurl จะไปที่ Index
-//Code ที่ commend เป็นแบบวิธีที่1
-
-/*
-func mux(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		indexHandler(w, r)
-	case "/about":
-		aboutHandler(w, r)
-	case "/login":
-		loginHandler(w, r)
-	default:
-		http.NotFound(w, r)
-		//notFoudHandler(w, r)
-	}
-}
-*/
-
-/*
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Index Page"))
-}
-*/
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Index Page"))
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("About Page"))
-}
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Login Page"))
-}
-func notFoudHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("404 Page Not Foud1"))
-}
+//Basic Middleware
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/login", loginHandler)
-
-	err := http.ListenAndServe(":8080", mux)
-	//h := http.HandlerFunc(mux)
-	//err := http.ListenAndServe(":8080", h)
+	h := logger(http.HandlerFunc(indexHandler))
+	err := http.ListenAndServe(":8080", h)
 	log.Println(err)
+}
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//http://localhost:8080/about?hello=111
+		log.Printf("r.URL.Path: %s", r.URL.Path)       //r.URL.Path: /about
+		log.Printf("r.RequestURI: %s", r.RequestURI)   //r.RequestURI: /about?hello=111
+		log.Printf("r.URL.Query(): %s", r.URL.Query()) //r.URL.Query(): map[hello:[111]]
+		t := time.Now()
+		h.ServeHTTP(w, r)
+		diff := time.Now().Sub(t)
+		log.Printf("path: %s, time: %d ms", r.URL.Path, diff/time.Microsecond)
+	})
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Index Page1"))
 }
