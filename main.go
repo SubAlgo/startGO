595d1869a1a9623c1d95d55f64278b7ec72b4892
kornchaval <kornchaval@gmail.com>
2017-08-26 01:56:01 +0700

    Config middleware manage same middleware

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 )
 
-//Config middleware 1
+//Config middleware manage same middleware
 func main() {
 	//http.DefaultServeMux()
-	http.HandleFunc("/", indexHandler)                                    //ใช้ HandleFunc เพราะ indexHandler เป็นเหมือน func ธรรมดาที่ต้องแปลงเป็น HandleFunc
-	http.Handle("/admin", allowRoleAdmin(http.HandlerFunc(adminHandler))) //ใช้ Handle เพราะ allowRoleAdmin retunr เป็น http.Handler
+	http.HandleFunc("/", indexHandler)                                        //ใช้ HandleFunc เพราะ indexHandler เป็นเหมือน func ธรรมดาที่ต้องแปลงเป็น HandleFunc
+	http.Handle("/admin", allowRole("admin")(http.HandlerFunc(adminHandler))) //ใช้ Handle เพราะ allowRoleAdmin retunr เป็น http.Handler
 	// --/admin จะผ่าน  middleware [allowRoleAdmin]
-	http.Handle("/stuff", allowRoleStuff(http.HandlerFunc(stuffHandler)))
+	http.Handle("/stuff", allowRole("stuff")(http.HandlerFunc(stuffHandler)))
 	//ส่วนของ Handle กับ HandleFunc จะเรียก DefultServeMux อยู่แล้ว
 	//เมื่อมีการใช้ DefuleServeMux http.ListenAndServe ตรง handler ก็ใส่ค่า nil ได้เลย เพราะมันจะไปเรียกจาก DefultServeMux ที่เราทำไว้ก่อนหน้านี้
 	err := http.ListenAndServe(":8080", nil)
@@ -25,6 +25,26 @@ func main() {
 
 type middleware func(http.Handler) http.Handler
 
+/*
+การทำงานของ allowRole คือ เมื่อมีการ request path (func main) จะเรียก func allowRole โดยมีการส่ง string เข้ามา
+โดย จะถูกเก็บไว้ใน ตัวแปร role แล้ว Handler เช่น (http.HandlerFunc(adminHandler)) จะถูกโยนใส่ func ตัวใน
+ต่อมาตัวแปร reqRole จะถูกสร้างมาเพื่อเก็บ Header สมมติ ที่ชื่ Role
+ถ้า path ที่ถูกเรียก header ไม่ตรงกับ role ที่ส่งเข้ามา (role string) ก็จะเข้าไม่ได้
+*/
+func allowRole(role string) middleware { //รับ role return minddleware
+	return func(h http.Handler) http.Handler { //Middleware
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			reqRole := r.Header.Get("Role")
+			if reqRole != "role" {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+/*-----------------	-----------------------
 //-------- Middleware จัดการ Admin --------
 func allowRoleAdmin(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +68,7 @@ func allowRoleStuff(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+----------------------------------------*/
 
 /*********************************
 *			HANDLER				 *
